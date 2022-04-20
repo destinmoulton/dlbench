5933d838d9bcd10ce81d9cbc67769f9bce5013e7
Destin Moulton <destin.moulton@gmail.com>
2022-04-20 14:08:11 -0600

    Added json structs

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
+
+const settingsFile = "settings.json"
+
+type JSONSections struct {
+	Domains []JSONDomain `json:"domains"`
+	Files   []JSONFile   `json:"files"`
+}
+
+type JSONDomain struct {
+	Domain string `json:"domain"`
+	Path   string `json:"path"`
+	Host   string `json:"host"`
+}
+
+type JSONFile struct {
+	Name string `json:"name"`
+	Size string `json:"size"`
+}
 
 func main() {
 	fmt.Println("test")
+	jsonFile, err := os.Open(settingsFile)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
 }
