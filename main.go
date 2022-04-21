599ce22262db903c67c48148a38ad3014c9abc36
Destin Moulton <destin.moulton@gmail.com>
2022-04-21 08:57:02 -0600

    Fixed bugs with passing/returning csv writer

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -8,13 +9,17 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const settingsFile = "settings.json"
-const downloadsDir = "downloads"
 
-type JSONSections struct {
+var settings JSONSettings
+
+type JSONSettings struct {
+	Config  JSONConfig   `json:"config"`
 	Domains []JSONDomain `json:"domains"`
 	Files   []JSONFile   `json:"files"`
 }
@@ -30,55 +35,87 @@ type JSONFile struct {
 	Size string `json:"size"`
 }
 
+type JSONConfig struct {
+	Rounds         int    `json:"rounds"`
+	DownloadFolder string `json:"download_folder"`
+	CSVFile        string `json:"csv_file"`
+}
+
 func main() {
-	var settings JSONSections
-
-	fmt.Println("test")
-	jsonFile, err := os.Open(settingsFile)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	json.Unmarshal(byteValue, &settings)
+	populateSettings(&settings)
+	csvFile, csvWriter := createCSVFile()
 
 	for _, domain := range settings.Domains {
 		fmt.Println("Testing Domain: " + domain.Domain + " on " + domain.Host)
+
 		for _, file := range settings.Files {
-			var url = domain.Domain + domain.Path + file.Name
-			fmt.Println("Downloading " + file.Name + " from " + domain.Domain)
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err)
-			}
-			defer resp.Body.Close()
+			var totalSeconds float64
+			var lastSize int64
+			cnt := 0
 
-			if resp.StatusCode != http.StatusOK {
-				fmt.Errorf("Error. Domain returned a %s status.", resp.Status)
-			} else {
-				fmt.Printf("Successfully downloaded.")
-			}
+			// Run for the number of rounds
+			for i := 0; i < settings.Config.Rounds; i++ {
+				var url = domain.Domain + domain.Path + file.Name
+				fmt.Println("Downloading " + file.Name + " from " + domain.Domain)
 
-			var domainDir = cleanDomainDir(domain.Domain)
-			createDownloadFolder(domainDir)
-			var outpath = filepath.Join(downloadsDir, domainDir, file.Name)
-			outfile, err := os.Create(outpath)
-			if err != nil {
-				fmt.Println(err)
-			}
-			defer outfile.Close()
+				startBench := time.Now()
+				resp, err := http.Get(url)
+				if err != nil {
+					fmt.Println(err)
+				}
+				endBench := time.Now()
+				elapsed := endBench.Sub(startBench)
+				totalSeconds += elapsed.Seconds()
 
-			_, err = io.Copy(outfile, resp.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
+				defer resp.Body.Close()
 
+				if resp.StatusCode != http.StatusOK {
+					fmt.Printf("Error. Domain returned a %s status.\n", resp.Status)
+				} else {
+					fmt.Printf("Successfully downloaded in %fs.\n", elapsed.Seconds())
+				}
+
+				domainDir := cleanDomainDir(domain.Domain)
+				createDownloadFolder(domainDir)
+				outpath := filepath.Join(settings.Config.DownloadFolder, domainDir, file.Name)
+				outfile, err := os.Create(outpath)
+				if err != nil {
+					fmt.Println(err)
+				}
+				defer outfile.Close()
+
+				_, err = io.Copy(outfile, resp.Body)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				info, err := os.Stat(outpath)
+				if err != nil {
+					fmt.Printf("Unable to stat %s\n", outpath)
+					os.Exit(-1)
+				}
+
+				lastSize = info.Size()
+
+				cnt++
+			}
+			kb := float64(lastSize) / (1 << 10)
+			avgTime := totalSeconds / float64(cnt)
+			avgRate := float64(kb) / float64(avgTime)
+			row := []string{
+				domain.Domain,
+				file.Name,
+				strconv.Itoa(cnt),
+				strconv.FormatFloat(kb, 'f', 2, 64) + "kB",
+				strconv.FormatFloat(avgTime, 'f', 2, 64) + "s",
+				strconv.FormatFloat(avgRate, 'f', 2, 64) + "kB/s",
+				domain.Host,
+			}
+			writeCSVRow(csvWriter, row)
 		}
 	}
+	csvWriter.Flush()
+	csvFile.Close()
 }
 
 // Clean the domain name for use as a directory
@@ -90,10 +127,58 @@ func cleanDomainDir(domain string) string {
 
 // Create a download folder
 func createDownloadFolder(subdir string) {
-	dir := filepath.Join(downloadsDir, subdir)
+	dir := filepath.Join(settings.Config.DownloadFolder, subdir)
 	err := os.MkdirAll(dir, 0755)
 	if err != nil {
-		fmt.Printf("Unable to create the directory %s", dir)
+		fmt.Printf("Unable to create the directory %s\n", dir)
 		os.Exit(-1)
 	}
+}
+
+func createCSVFile() (*os.File, *csv.Writer) {
+	csvFile, err := os.Create(settings.Config.CSVFile)
+
+	if err != nil {
+		fmt.Printf("Unable to create the CSV file %s\n", settings.Config.CSVFile)
+		os.Exit(-1)
+	}
+	csvWriter := csv.NewWriter(csvFile)
+
+	// Write the header row
+	header := []string{
+		"Domain",
+		"File",
+		"Rounds",
+		"File Size (kB)",
+		"Avg. Time (s)",
+		"Avg. Rate (kB/s)",
+		"Host",
+	}
+	writeCSVRow(csvWriter, header)
+	return csvFile, csvWriter
+}
+
+func writeCSVRow(csvWriter *csv.Writer, row []string) {
+	fmt.Println(&csvWriter)
+	err := csvWriter.Write(row)
+	if err != nil {
+		fmt.Printf("Unable to write %v to CSV\n", row)
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+// Populate the settings struct from the settings json file
+func populateSettings(settings *JSONSettings) {
+	jsonFile, err := os.Open(settingsFile)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &settings)
 }
